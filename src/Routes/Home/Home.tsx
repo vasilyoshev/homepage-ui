d6e55850b392e303d5b818d85d447b0fe72dd0ac
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-02-05 23:40:47 +0200

    Fix issue with CalendarWidget component and remove fragment notations

--- src/Routes/Home/Home.tsx
+++ src/Routes/Home/Home.tsx
@@ -2,5 +2,5 @@ import './Home.scss';
 import { CalendarWidget } from 'widgets/CalendarWidget';
 
 export const Home: React.FC = () => {
-  return <> <CalendarWidget /> </>;
+  return <CalendarWidget />;
 };
