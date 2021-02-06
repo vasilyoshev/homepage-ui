1447f5f52595525f95b529c072b853631a50199a
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-02-06 15:02:40 +0200

    Set calendar widget to static mode

--- src/widgets/CalendarWidget.tsx
+++ src/widgets/CalendarWidget.tsx
@@ -1,27 +1,16 @@
-/* eslint-disable quotes */
-import React, { useState } from "react";
-import DateFnsUtils from "@date-io/date-fns";
-import { DatePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
+import React from 'react';
+import DateFnsUtils from '@date-io/date-fns';
+import { DatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
 
 export const CalendarWidget: React.FC = () => {
-  const [date, changeDate] = useState(new Date());
+  const date = new Date();
   return (
     <MuiPickersUtilsProvider utils={DateFnsUtils}>
       <DatePicker
-        autoOk
-        variant="static"
-        openTo="year"
-        value={date}
-        onChange={() => changeDate}
-      />
-
-      <DatePicker
-        autoOk
         orientation="landscape"
         variant="static"
-        openTo="date"
         value={date}
-        onChange={() => changeDate}
+        onChange={() => null}
       />
     </MuiPickersUtilsProvider>
   );
