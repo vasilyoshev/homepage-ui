a85814ebfc8414f256f572bc95590e6ae7a765c7
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-02-08 23:15:27 +0200

    Change TodoListProps, because the component is TodoList, not TodoItem.

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-type TodoItemProps = {
+type TodoListProps = {
     items: Array<{id: string, text: string}>;
 }
 
-export const TodoList: React.FC<TodoItemProps> = ({ items }) => {
+export const TodoList: React.FC<TodoListProps> = ({ items }) => {
   return <ul>
     {items.map(todoItem => (
       <li key={todoItem.id}>{todoItem.text}</li>
