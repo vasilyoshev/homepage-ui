ee45d2a7c1d1d207c0af89ae21397bd505d0a1dd
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-03-31 20:07:05 +0300

    Set Material-UI imports onto one line

--- src/components/CryptoCurrencyTable/CryptoCurrencyTable.tsx
+++ src/components/CryptoCurrencyTable/CryptoCurrencyTable.tsx
@@ -1,11 +1,5 @@
 import React from 'react';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import TableHead from '@material-ui/core/TableHead';
+import { Table, TableHead, TableBody, TableCell, TableContainer, TableRow, Paper } from '@material-ui/core';
 import { CryptoCurrencyWidgetTable } from 'interfaces';
 import styles from './CryptoCurrencyTable.module.scss';
 
