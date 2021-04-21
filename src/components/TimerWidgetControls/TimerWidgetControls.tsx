be8ab4802d089cabb4cca084150f08937d555921
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-04-22 00:36:52 +0300

    Change 'className' and remove flex property

--- src/components/TimerWidgetControls/TimerWidgetControls.tsx
+++ src/components/TimerWidgetControls/TimerWidgetControls.tsx
@@ -5,7 +5,7 @@ import styles from './TimerWidgetControls.module.scss';
 
 export const TimerWidgetControls: React.FC<TimerWidgetControlsProps> = ({ timerOn, time, startTimer, stopTimer, resetTimer, resumeTimer }) => {
   return (
-    <section className={styles.buttons}>
+    <section className={styles.buttonsContainer}>
       {!timerOn && time === 0 && (
         <Button variant="outlined" color="primary" onClick={startTimer}>Start</Button>
       )}
