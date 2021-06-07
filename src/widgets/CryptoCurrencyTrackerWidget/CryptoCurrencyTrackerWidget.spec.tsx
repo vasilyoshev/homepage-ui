ee1949acb4a390c2705871a33f11d63738d8ba37
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-06-07 20:41:04 +0300

    Simulate user input for CryptoWidget

--- src/widgets/CryptoCurrencyTrackerWidget/CryptoCurrencyTrackerWidget.spec.tsx
+++ src/widgets/CryptoCurrencyTrackerWidget/CryptoCurrencyTrackerWidget.spec.tsx
@@ -1,10 +1,17 @@
-import { render } from 'utils';
+import { render, screen, fireEvent } from 'utils';
 import { CryptoCurrencyTrackerWidget } from './CryptoCurrencyTrackerWidget';
 
 describe('CryptoCurrencyTrackerWidget', () => {
   it('renders successfully', () => {
     const { asFragment } = render(<CryptoCurrencyTrackerWidget />);
-    
     expect(asFragment()).toMatchSnapshot();
+  });
+  it('should display currency searched by the user', async() => {
+    render(<CryptoCurrencyTrackerWidget />);
+    const input = screen.getByRole('textbox') as HTMLInputElement;
+    fireEvent.change(input, { target: { value: 'bitcoin'}});
+    expect(input.value).toBe('bitcoin');
+    const element = await screen.findByText(/bitcoin/i);
+    expect(element).toBeInTheDocument();
   });
 });
