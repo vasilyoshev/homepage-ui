75e4e2302e84abfbd0620f4e482cbada8e80bc6a
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-08-06 13:42:25 +0300

    Set MockDate

--- src/Routes/Dashboard/Dashboard.spec.tsx
+++ src/Routes/Dashboard/Dashboard.spec.tsx
@@ -1,10 +1,12 @@
 import { Dashboard } from './Dashboard';
 import { render } from 'utils';
+import MockDate from 'mockdate';
 
 describe('Dashboard', () => {
+  afterAll(() => MockDate.reset());
   it('renders successfully', () => {
     const { asFragment } = render(<Dashboard />);
-
+    MockDate.set('2021');
     expect(asFragment()).toMatchSnapshot();
   });
 });
