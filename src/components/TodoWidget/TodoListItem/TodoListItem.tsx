cb6e67e66553e3454014c9c8db052772e2dcaf05
DobriJS <53184157+DobriJS@users.noreply.github.com>
2021-08-13 12:45:15 +0300

    Update Todo

--- src/components/TodoWidget/TodoListItem/TodoListItem.tsx
+++ src/components/TodoWidget/TodoListItem/TodoListItem.tsx
@@ -2,40 +2,35 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { TodoListItemProps } from 'interfaces';
 import { removeTodo, completeTodo } from 'slices';
-import { Checkbox, InputLabel, Typography, IconButton } from '@material-ui/core';
-import DeleteIcon from '@material-ui/icons/Delete';
+import { ListItemSecondaryAction, ListItem, ListItemText, IconButton, ListItemIcon, Checkbox } from '@material-ui/core';
+import { DeleteOutlineRounded } from '@material-ui/icons';
 import styles from './TodoListItem.module.scss';
 
-export const TodoListItem: React.FC<TodoListItemProps> = ({
-  todo,
-}) => {
+export const TodoListItem: React.FC<TodoListItemProps> = ({ todo }) => {
   const dispatch = useDispatch();
   return (
-    <InputLabel
-      className={todo.completed ? styles.completed : ''}
-    >
-      <Checkbox
-        checked={todo.completed}
-        color="primary"
-        edge="start"
-        disableRipple
-        onChange={() => dispatch(completeTodo(todo.id))}
-      />
-      <Typography
-        className={styles.todoText}
-        display="inline"
-        color="primary">
-        {todo.text}
-      </Typography>
-      <IconButton
-        data-testid="removeTodoItem"
-        aria-label="delete"
-        onClick={() => dispatch(removeTodo(todo.id))}
-      >
-        <DeleteIcon
-          fontSize="small"
+    <ListItem className={styles.listItem}>
+      <ListItemIcon>
+        <Checkbox
+          checked={todo.completed}
+          color="primary"
+          edge="start"
+          disableRipple
+          onChange={() => dispatch(completeTodo(todo.id))}
         />
-      </IconButton>
-    </InputLabel>
+      </ListItemIcon>
+      <ListItemText
+        className={todo.completed ? styles.completed : ''}
+        primary={todo.text}
+      />
+      <ListItemSecondaryAction>
+        <IconButton
+          edge="end"
+          aria-label="delete"
+          onClick={() => dispatch(removeTodo(todo.id))}>
+          <DeleteOutlineRounded />
+        </IconButton>
+      </ListItemSecondaryAction>
+    </ListItem>
   );
 };
--- src/widgets/index.ts
+++ src/widgets/index.ts
@@ -3,3 +3,4 @@ export * from './TodoWidget/TodoWidget';
 export * from './CryptoCurrencyTrackerWidget/CryptoCurrencyTrackerWidget';
 export * from './TimerWidget/TimerWidget';
 export * from './WeatherWidget/WeatherWidget';
+export * from './TodoWidget/TodoWidget';
