a7ba3e904848b705b322058168a70483ae9eb479
DobriJS <53184157+DobriJS@users.noreply.github.com>
2022-05-12 14:55:14 +0300

    Change imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import { Home, SignUp, SignIn } from 'Routes';
+import { Home } from './Routes/Home/Home';
+import { SignUp } from './Routes/SignUp/SignUp';
+import { SignIn } from './Routes/SignIn/SignIn';
 
 export const App: React.FC = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/signin" element={<SignIn />} />
+      <Route path='/' element={<Home />} />
+      <Route path='/signup' element={<SignUp />} />
+      <Route path='/signin' element={<SignIn />} />
     </Routes>
   );
 };
--- src/Routes/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './Home/Home';
-export { SignUp } from './SignUp/SignUp';
-export * from './SignIn/SignIn';
