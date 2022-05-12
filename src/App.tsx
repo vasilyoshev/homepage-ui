f571d14c6bc77b5412005c56dab1908df05ba076
DobriJS <53184157+DobriJS@users.noreply.github.com>
2022-05-12 15:01:23 +0300

    remove signup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { Home } from './Routes/Home/Home';
-import { SignUp } from './Routes/SignUp/SignUp';
 import { SignIn } from './Routes/SignIn/SignIn';
 
 export const App: React.FC = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='/signup' element={<SignUp />} />
       <Route path='/signin' element={<SignIn />} />
     </Routes>
   );
