dccda8006717c5cd064167d946a3a0e3619b1f2b
DobriJS <53184157+DobriJS@users.noreply.github.com>
2022-05-28 15:14:47 +0300

    ImageList

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,5 @@
 export * from './MoviesApp/SearchBar/SearchBar';
 export * from './MoviesApp/HeroSection/HeroSection';
+export * from './MoviesApp/MovieItem';
+export * from './MoviesApp/MovieList';
+export * from './MoviesApp/Movies';
