64b3988f3f49a461c1513faa3e17fbeeaa9dae48
DobriJS <53184157+DobriJS@users.noreply.github.com>
2022-05-29 15:29:29 +0300

    Complete migration to NextJS

--- src/schemas/yupResolver.ts
+++
@@ -1,6 +0,0 @@
-import * as yup from 'yup';
-
-export const schema = yup.object().shape({
-  username: yup.string().required().min(5).max(25),
-  password: yup.string().required().min(8).max(120),
-});
